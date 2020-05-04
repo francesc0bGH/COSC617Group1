f5c29c1d078f3eccfdfb33d42a56a3681bcc3dcf
Donnie Brown <dpbrownjr@gmail.com>
2020-05-03 22:48:08 -0400

    Create totalsports.sh

---
+++ totalsports.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#   A simple script to pull an updated copy of the git repo and start the node server
+
+if [ ! -d "$GITPROJECTS/COSC617Group1" ]
+then
+    git clone --single-branch --branch dev https://github.com/francesc0bGH/COSC617Group1.git
+    cd $GITPROJECTS/COSC617Group1
+    npm install
+    npm run devStart
+else
+    git pull
+    npm install
+    npm run devStart
+fi
